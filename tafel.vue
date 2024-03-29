8da1a63997246174e668d067f2c786137addb40b
chrille69 <christian@lehrer-hoffmann.de>
2024-03-29 01:32:19 +0000

    TODO: Skalierungsproblem des Moveable

--- tafel.vue
+++ tafel.vue
@@ -1,24 +1,27 @@
 <template>
-    <svg ref="svg_comp" id="tafel" xmlns="http://www.w3.org/2000/svg" style="touch-action: none"
-        @mousedown="startWork" @mousemove="furtherWork" @mouseup="endWork" 
-        @touchstart="startWork" @touchmove="furtherWork" @touchend="endWork" 
-        >
-        <text v-if="false" x="20" y="20" style="fill: red;">Touchradius:{{ touchradius }}</text>
-        <g ref="group_comp" id='container' :style="transformstyle">
-            <vorlagenvue :vorlagen="vorlagen" />
-            <bildervue :bilder="bilder" />
-            <pfadevue :pfade="pfade" />
-        </g>
-        <rect v-if="zeigeRadierer" ref="radiergummi" :="radiergummiBox" style="stroke: red; stroke-width: 2px; fill: none;" />
-        <geodreieck  ref="geodreieck_el"  v-show="props.config.geodreieckaktiv">
-            <path id="verschiebegriff" style="pointer-events: bounding-box; fill: #0000ff; stroke-width:.26458" d="m80 40-3 3h2v4h-4v-2l-3 3 3 3v-2h4v4h-2l3 3 3-3h-2v-4h4v2l3-3-3-3v2h-4v-4h2z"/>
-            <path id="drehgriff" style="pointer-events: bounding-box; fill: #ff0000; stroke-width:.026458" d="m79.368 11 c-.21279.0071-.3996.02137-.53513.04429-1.6929.26686-3.1357.95898-4.3198 2.0598-1.2426 1.1759-2.0097 2.5607-2.385 4.3203-.05013.22512-.06674.22488-.75887.25004l-.70905.02491 1.3175 1.9768c.71729 1.0924 1.3262 1.9763 1.3512 1.9763.02497 0 .63391-.88387 1.3512-1.9763l1.3175-1.9768-.91756-.02491-.9171-.02492.04982-.22512c.317-1.3261 1.4682-2.844 2.719-3.5863.98397-.58367 2.4351-.91716 3.4857-.8087 1.2926.14182 2.5934.65881 3.2606 1.2926l.35891.34184.67537-.67538.66707-.67583-.33353-.29156c-1.0091-.89228-2.4937-1.6431-3.7279-1.9016-.43775-.09378-1.3116-.14056-1.95-.11948zm7.3045 5.3315c-.02517 0-.63438.88433-1.3517 1.9768l-1.3175 1.9763 1.8347.04983-.04982.22513c-.317 1.3259-1.4675 2.8438-2.7186 3.5858-.98397.58386-2.4351.91762-3.4857.80916-1.2926-.14183-2.5939-.65882-3.2611-1.2926l-.35844-.34184-.67583.67583-.66707.67537.33353.29202c.58386.51714 1.7016 1.2089 2.4021 1.4924 2.7188 1.1008 5.9207.48388 8.0976-1.551 1.2676-1.1841 2.035-2.5688 2.4187-4.3452.04994-.22512.06674-.22488.75887-.25004l.70859-.02492-1.3175-1.9768c-.71729-1.0924-1.3256-1.9763-1.3507-1.9763z"/>
-        </geodreieck>
-    </svg>
+    <div>
+        <div ref="moveable_comp"></div>
+        <svg ref="svg_comp" id="tafel" xmlns="http://www.w3.org/2000/svg" :style="svgstyle"
+            @mousedown="startWork" @mousemove="furtherWork" @mouseup="endWork" 
+            @touchstart="startWork" @touchmove="furtherWork" @touchend="endWork" 
+            >
+            <text v-if="false" x="20" y="20" style="fill: red;">Touchradius:{{ touchradius }}</text>
+            <g ref="group_comp" id='container' :style="groupstyle">
+                <vorlagenvue :vorlagen="vorlagen" />
+                <bildervue :bilder="bilder" />
+                <pfadevue :pfade="pfade" />
+            </g>
+            <rect v-if="zeigeRadierer" ref="radiergummi" :="radiergummiBox" style="stroke: red; stroke-width: 2px; fill: none;" />
+            <geodreieck  ref="geodreieck_el"  v-show="props.config.geodreieckaktiv">
+                <path id="verschiebegriff" style="pointer-events: bounding-box; fill: #0000ff; stroke-width:.26458" d="m80 40-3 3h2v4h-4v-2l-3 3 3 3v-2h4v4h-2l3 3 3-3h-2v-4h4v2l3-3-3-3v2h-4v-4h2z"/>
+                <path id="drehgriff" style="pointer-events: bounding-box; fill: #ff0000; stroke-width:.026458" d="m79.368 11 c-.21279.0071-.3996.02137-.53513.04429-1.6929.26686-3.1357.95898-4.3198 2.0598-1.2426 1.1759-2.0097 2.5607-2.385 4.3203-.05013.22512-.06674.22488-.75887.25004l-.70905.02491 1.3175 1.9768c.71729 1.0924 1.3262 1.9763 1.3512 1.9763.02497 0 .63391-.88387 1.3512-1.9763l1.3175-1.9768-.91756-.02491-.9171-.02492.04982-.22512c.317-1.3261 1.4682-2.844 2.719-3.5863.98397-.58367 2.4351-.91716 3.4857-.8087 1.2926.14182 2.5934.65881 3.2606 1.2926l.35891.34184.67537-.67538.66707-.67583-.33353-.29156c-1.0091-.89228-2.4937-1.6431-3.7279-1.9016-.43775-.09378-1.3116-.14056-1.95-.11948zm7.3045 5.3315c-.02517 0-.63438.88433-1.3517 1.9768l-1.3175 1.9763 1.8347.04983-.04982.22513c-.317 1.3259-1.4675 2.8438-2.7186 3.5858-.98397.58386-2.4351.91762-3.4857.80916-1.2926-.14183-2.5939-.65882-3.2611-1.2926l-.35844-.34184-.67583.67583-.66707.67537.33353.29202c.58386.51714 1.7016 1.2089 2.4021 1.4924 2.7188 1.1008 5.9207.48388 8.0976-1.551 1.2676-1.1841 2.035-2.5688 2.4187-4.3452.04994-.22512.06674-.22488.75887-.25004l.70859-.02492-1.3175-1.9768c-.71729-1.0924-1.3256-1.9763-1.3507-1.9763z"/>
+            </geodreieck>
+        </svg>
+    </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import geodreieck from './geodreieck.vue'
 import pfadevue from './pfade.vue'
 import bildervue from './bilder.vue'
@@ -27,6 +30,7 @@ import vorlagenvue from './vorlagen.vue'
 const props = defineProps(['config'])
 const emit = defineEmits(['hatgemalt'])
 
+const moveable_comp = ref(null)
 const svg_comp = ref(null)
 const group_comp = ref(null)
 const pfade = ref([])
@@ -38,10 +42,22 @@ let neuerPfad = null
 const items = computed(() => [...pfade.value,...bilder.value,...vorlagen.value])
 const transform = ref({x: 0, y: 0, scale: 1})
 const transformOrigin = ref({x: window.innerWidth/2, y: window.innerHeight/2})
-const transformstyle = computed(() => {
+const groupstyle = computed(() => {
     return {
         transform: `scale(${transform.value.scale}) translate(${transform.value.x}px, ${transform.value.y}px)`,
         transformOrigin: `${transformOrigin.value.x}px ${transformOrigin.value.y}px`
+    }
+})
+const moveablestyle = computed(() => {
+    return {
+        transformOrigin: `${transformOrigin.value.x}px ${transformOrigin.value.y}px`
+    }
+})
+const svgstyle = computed(() => {
+    return {
+        touchAction: 'none',
+        height: '100%',
+        width: '100%',
     }
 })
 onresize = () => {
@@ -185,7 +201,6 @@ function startDraw(e) {
             'vector-effect': "non-scaling-stroke"
         },
         transform: '',
-        el: null,
         id: ++itemid,
     }
     if (props.config.tool == 'stift') 
@@ -310,14 +325,16 @@ function commit() {
 //
 //////////////////////////////////////////////////////////////
 
+let moveable = null
+let selecto = null
 let targets = []
 function setTargets(nextTargets) {
     targets = nextTargets;
     moveable.target = targets;
 }
 
-const moveable = new Moveable(document.body, {
-    container: svg_comp.value,
+onMounted(() => {
+moveable = new Moveable(moveable_comp.value, {
     target: targets,
     draggable: true,
     rotatable: true,
@@ -330,7 +347,7 @@ const moveable = new Moveable(document.body, {
 .on("renderEnd", (e) => { commit() })
 .on("renderGroupEnd", (e) => { commit() })
 .on("render", (e) => {
-    if(e.target.id == 'geodreieck') {
+    if (e.target.id == 'geodreieck') {
         geodreieck_el.value.setTransform(e.transformObject)
         return
     }
@@ -339,7 +356,7 @@ const moveable = new Moveable(document.body, {
 })
 .on("renderGroup", (ev) => {
     ev.events.forEach(e => {
-        if(e.target.id == 'geodreieck') {
+        if (e.target.id == 'geodreieck') {
             geodreieck_el.value.setTransform(e.transformObject)
             return
         }
@@ -348,7 +365,7 @@ const moveable = new Moveable(document.body, {
     });
 })
 
-const selecto = new Selecto({
+selecto = new Selecto({
     container: svg_comp.value,
     selectByClick: true,
     selectFromInside: false,
@@ -391,6 +408,7 @@ const selecto = new Selecto({
         });
     }
     setTargets(e.selected)
+})
 })
 
 ///////////////////////////////////////////////////////////
@@ -454,21 +472,19 @@ function copySelected() {
     let copyitems = pfade.value.filter((item) => targets.indexOf(item.el) >= 0 )
     for (let item of copyitems) {
         let newitem = {...item}
-        newitem.el = ref(null)
+        newitem.points = [...item.points]
         newitem.id = ++itemid
         pfade.value.unshift(newitem)
     }
     copyitems = bilder.value.filter((item) => targets.indexOf(item.el) >= 0 )
     for (let item of copyitems) {
         let newitem = {...item}
-        newitem.el = ref(null)
         newitem.id = ++itemid
         bilder.value.unshift(newitem)
     }
     copyitems = vorlagen.value.filter((item) => targets.indexOf(item.el) >= 0 )
     for (let item of copyitems) {
         let newitem = {...item}
-        newitem.el = ref(null)
         newitem.id = ++itemid
         vorlagen.value.unshift(newitem)
     }
@@ -476,7 +492,11 @@ function copySelected() {
 }
 
 function exportJson() {
-    return JSON.stringify({bilder: bilder.value, pfade:pfade.value, vorlagen: vorlagen.value})
+    return JSON.stringify({
+        bilder: bilder.value,
+        pfade:pfade.value,
+        vorlagen: vorlagen.value
+    })
 }
 
 function importJson(jsonstr) {
@@ -495,10 +515,10 @@ function zoomin() { transform.value.scale *= 1.1 }
 function zoomreset() { transform.value.scale = 1 }
 
 defineExpose({
-    deleteSelected,
-    copySelected,
     neuesBild,
     neueVorlage,
+    deleteSelected,
+    copySelected,
     exportJson,
     importJson,
     undo,
